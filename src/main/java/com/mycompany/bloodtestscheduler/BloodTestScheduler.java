b91c5a4fa92f5799ca8a65a52779497e7afe1a3a
Shemen Eroje <seroje777@gmail.com>
2025-03-15 23:10:27 +0000

    First Commit

---
+++ src/main/java/com/mycompany/bloodtestscheduler/BloodTestScheduler.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bloodtestscheduler;
+
+/**
+ *
+ * @author shemeneroje
+ */
+public class BloodTestScheduler {
+
+    public static void main(String[] args) {
+        BTSGUI myGUI = new BTSGUI();
+        myGUI.setVisible(true);
+    }
+}
