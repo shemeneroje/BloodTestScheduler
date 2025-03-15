40ccdb7587697f784fa2095650fb5390192799b2
Shemen Eroje <seroje777@gmail.com>
2025-03-15 23:53:43 +0000

    Third Commit

--- src/main/java/com/mycompany/bloodtestscheduler/Scheduler.java
+++ src/main/java/com/mycompany/bloodtestscheduler/Scheduler.java
@@ -8,6 +8,42 @@ package com.mycompany.bloodtestscheduler;
  *
  * @author shemeneroje
  */
-public class Scheduler{
-    
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
+public class Scheduler implements TestScheduler {
+    private PriorityQueue<Patient> queue;
+
+    public Scheduler() {
+        queue = new PriorityQueue<>(new Comparator<Patient>() {
+            @Override
+            public int compare(Patient p1, Patient p2) {
+                if (p1.getPriorityValue() != p2.getPriorityValue()) {
+                    return Integer.compare(p2.getPriorityValue(), p1.getPriorityValue());
+                }
+                if (p1.isFromHospital() && !p2.isFromHospital()) {
+                    return -1;
+                }
+                if (!p1.isFromHospital() && p2.isFromHospital()) {
+                    return 1;
+                }
+                return Integer.compare(p2.getAge(), p1.getAge());
+            }
+        });
+    }
+
+    @Override
+    public void addPatient(Patient p) {
+        queue.add(p);
+    }
+
+    @Override
+    public Patient getNextPatient() {
+        return queue.poll();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
 }
