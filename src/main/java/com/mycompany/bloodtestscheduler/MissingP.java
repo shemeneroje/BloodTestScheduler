df17702437e1da913a34c54e1e9998352f02677c
Shemen Eroje <seroje777@gmail.com>
2025-03-16 00:08:12 +0000

    Fourth Commit

--- src/main/java/com/mycompany/bloodtestscheduler/MissingP.java
+++ src/main/java/com/mycompany/bloodtestscheduler/MissingP.java
@@ -8,6 +8,28 @@ package com.mycompany.bloodtestscheduler;
  *
  * @author shemeneroje
  */
-public class MissingP{
-    
+import java.util.LinkedList;
+
+public class MissingP implements PTracker {
+    private LinkedList<Patient> noShowList;
+
+    public MissingP() {
+        noShowList = new LinkedList<>();
+    }
+
+    @Override
+    public void addNoShow(Patient p) {
+        if (noShowList.size() == 5) {
+            noShowList.removeFirst();
+        }
+        noShowList.addLast(p);
+    }
+
+    @Override
+    public void showNoShows() {
+        System.out.println("Last 5 No-Shows:");
+        for (Patient p : noShowList) {
+            System.out.println(p);
+        }
+    }
 }
